e1d2509f91f0b24355ddca13b31e149f4d76ebb9
Jeremy <jeremy.jacobskind@gmail.com>
2014-10-09 15:15:03 -0400

    Refined dropdown menu and established partial list functionality.

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,4 +1,7 @@
-var trip = {name:"Trip1"};
+var trip = {name:"Trip1", days: [{restaurants: [], activities: []}]};
+
+var whichDay = 1;
+var numDays = 1;    //!!!!! To be changed so that it gets set by length of days in object supplied by database!!!!!
 
 function initialize_gmaps() {
  
@@ -29,15 +32,12 @@ function initialize_gmaps() {
 
   $("#map-canvas").append("map");
 }
-
-
  
 $(document).ready(function() {
   initialize_gmaps();
 
-  var numDays = 3;
 
-  //Dropdown item click handler
+  //Event handler for when dropdown item is clicked
   $(".dropdown-menu").on('click', 'a', function(event) {
     event.preventDefault();
     var top_field = $(this).closest("div").children().first();
@@ -47,19 +47,41 @@ $(document).ready(function() {
 
   });
 
-  // "Add" button click handler
+  //Event handler for when "Add" (hotel/activity/restaurant) button is clicked
   $(".btn-primary").on("click", function() {
     event.preventDefault();
-    var text = $(this).prev().children("button:first").text()
+    var topButton = $(this).prev().children("button:first");
+    var id = topButton.attr("data-id");
+    var whichButton = $(this).attr("data-id");
+    var current_day = trip.days[whichDay-1];
+    if(id===undefined || whichDay<1 || whichDay>numDays) {  //handles when "Add" is clicked, but nothing is selected
+      console.log("Invalid entry!");
+    }
+    else if(whichButton==="hotels_add") {
+      current_day.hotel=id;
+    }
+    else if(whichButton==="activities_add") {
+      if(current_day.activities.indexOf(id)===-1) {
+        current_day.activities.push(id);
+      }
+    }
+    else if(whichButton==="restaurants_add") {
+      if(current_day.restaurants.length<3) {
+        current_day.restaurants.push(id);
+      }
+    }
+    topButton.text($(this).parent().children("h2").text());
   });
 
-  // "Add Day" button click handler
-  $("#add_day").on('click', 'button', function(event) {
+
+  //Event handler for when "Add Day" button is clicked
+  $("#add_day_group").on('click', 'button', function(event) {
     event.preventDefault();
     numDays++;
-    var dayBar = $("#add_day").parent().children().first();
+    trip.days.push({restaurants: [], activities: []});
+    var dayBar = $("#add_day_group").parent().children().first();
     if(numDays<5) {
-      dayBar.append('<button type="button" class="btn btn-default">Day ' + numDays + '</button>');
+      dayBar.append('<button type="button" class="btn btn-default day" data-id="'+ numDays + '">Day ' + numDays + '</button>');
     }
     else if (numDays===5) {
       //clear div and insert dropdown menu
@@ -68,4 +90,44 @@ $(document).ready(function() {
     }
   });
 
+  $(".days_group").on('click', 'button', function() {
+   event.preventDefault();
+   whichDay = $(this).data("id");
+   $(this).parent().children().removeClass("btn-primary");
+   $(this).addClass("btn-primary");
+
+  var current_day = trip.days[whichDay-1];
+  var list = ["<h2>Plan for Day " + whichDay + "</h2><ul>"];
+  if(current_day.hotel!==undefined) {
+    list.push("<li><h3>Hotel</h3><p>" + getName("hotel", current_day.hotel) + "</p></li>");
+  }
+  $("#dynamic_fill").html("");
+  $("#dynamic_fill").append(list.join(""));
+
+ });
+
 }); 
+
+var getName = function(type, id) {
+  if(type==="hotel") {
+    for(var i=0, len=all_hotels.length; i<len;i++) {
+      if(all_hotels[i]._id===id) {
+        return all_hotels[i].name;
+      }
+    }
+  }
+  else if(type==="activity") {
+    for(var i=0, len=all_activities.length; i<len;i++) {
+      if(all_activities[i]._id===id) {
+        return all_activities[i].name;
+      }
+    }
+  }
+  else if(type==="restaurant") {
+    for(var i=0, len=all_restaurants.length; i<len;i++) {
+      if(all_restaurants[i]._id===id) {
+        return all_restaurants[i].name;
+      }
+    }
+  }
+};
