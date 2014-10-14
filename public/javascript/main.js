d0df7df8f06ea07ccb53cd3de3967d356c16a5ec
Jeremy <jeremy.jacobskind@gmail.com>
2014-10-14 10:43:28 -0400

    Added responsive styling, day removal not working yet

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -3,8 +3,24 @@ var trip = {name:"Trip1", days: [{restaurants: [], activities: []}]};
 var whichDay = 1;
 var numDays = 1;    //!!!!! To be changed so that it gets set by length of days in object supplied by database!!!!!
 var all_markers =[{}];
-// var all_markers[whichDay-1] = all_markers[0];
+// var day_markers = all_markers[0];
+var day_markers = all_markers[whichDay-1];
 
+
+var addMarker = function(id, type) {
+    var location = getProperty(type, id, "location", true);
+    var marker = new google.maps.Marker({
+        position: location,
+        title: getProperty(type, id, "name")
+    });
+    day_markers[id]=marker;
+    // console.log(markerBounds);
+    // markerBounds.extend(location);
+    // var curMap = $("#map");
+    // curMap.setCenter(markerBounds.getCenter(), curMap.getBoundsZoomLevel(markerBounds));
+    marker.setMap(map);
+    
+};
 
 function initialize_gmaps() {
  
@@ -24,12 +40,13 @@ function initialize_gmaps() {
   // initialize a new Google Map with the options
   map = new google.maps.Map(map_canvas_obj, mapOptions);
   
+  markerBounds = new google.maps.LatLngBounds();  //GLOBAL VARIABLE
   $("#map-canvas").append("map");
 }
  
 $(document).ready(function() {
   initialize_gmaps();
-  populateList("complete");
+  populateList();
 
 
   //Event handler for when dropdown item is clicked
@@ -56,8 +73,8 @@ $(document).ready(function() {
     }
     else if(whichButton==="hotels_add") {
       if(!!current_day.hotel) {
-        all_markers[whichDay-1][current_day.hotel].setMap(null);
-        delete all_markers[whichDay-1][current_day.hotel];
+        day_markers[current_day.hotel].setMap(null);
+        delete day_markers[current_day.hotel];
       }
       current_day.hotel=id;
       populateList();
@@ -81,13 +98,8 @@ $(document).ready(function() {
       }
     }
     topButton.text($(this).parent().children("h2").text());
-    var marker = new google.maps.Marker({
-        position: getLocation(type, id),
-        title: getProperty(type, id, "name")
-    });
-    if(!all_markers[whichDay-1].hasOwnProperty(id) || all_markers[whichDay-1][id].map===null) {
-      all_markers[whichDay-1][id]=marker;
-      marker.setMap(map);
+    if(!day_markers.hasOwnProperty(id) || day_markers[id].map===null) {
+      addMarker(id, type);
     }
   });
 
@@ -99,28 +111,22 @@ $(document).ready(function() {
     trip.days.push({restaurants: [], activities: []});
     all_markers.push({});
     var dayBar = $("#add_day_group").parent().children().first();
-    // if(numDays<5) {
-      dayBar.append('<button type="button" class="btn btn-default day" data-id="'+ numDays + '">Day ' + numDays + '</button>');
-    // }
-    // else if (numDays===5) {
-      //clear div and insert dropdown menu
-    // } else {
-      //add iitem to dropdown menu
-    // }
+    dayBar.append('<button type="button" class="btn btn-default day" data-id="'+ numDays + '">Day ' + numDays + '</button>');
   });
 
+  //Event handler for when day is changed
   $(".days_group").on('click', 'button', function() {
     event.preventDefault();
-    for(var key in all_markers[whichDay-1]) {
-      if(all_markers[whichDay-1].hasOwnProperty(key)) {
-        all_markers[whichDay-1][key].setMap(null);
+    for(var key in day_markers) {
+      if(day_markers.hasOwnProperty(key)) {
+        day_markers[key].setMap(null);
       }
     }
     whichDay = $(this).data("id");
-    all_markers[whichDay-1] = all_markers[whichDay-1];
-    for(var key in all_markers[whichDay-1]) {
-      if(all_markers[whichDay-1].hasOwnProperty(key)) {
-        all_markers[whichDay-1][key].setMap(map);
+    day_markers = all_markers[whichDay-1];
+    for(var key in day_markers) {
+      if(day_markers.hasOwnProperty(key)) {
+        day_markers[key].setMap(map);
       }
     }
     $(this).parent().children().removeClass("btn-primary");
@@ -134,71 +140,91 @@ $(document).ready(function() {
 
     var type = $(this).attr("data-type");
     var id = $(this).attr("data-id");
-    var curDay = trip.days[whichDay-1];
-
-    if(type==="hotel") {
-      curDay.hotel = undefined;
+    var searchArr=[];
+    var times_found = 0;
+    switch(type) {
+      case "hotel":
+        trip.days[whichDay-1].hotel=undefined;
+        break;
+      case "activity":
+        searchArr = trip.days[whichDay-1].activities;
+        var searchKey = "activities";
+        break;
+      case "restaurant":
+        searchArr = trip.days[whichDay-1].restaurants;
+        searchKey = "restaurants";
+        break;
     }
-    else if(type==="activity") {
-      var activities = trip.days[whichDay-1].activities;
-      for(i=0, len=activities.length; i<len; i++) {
-        if(id===activities[i]) {
-          curDay.activities = activities.slice(0,i).concat(activities.slice(i+1));
-        }
-      }
-    }
-    else if(type==="restaurant") {
-      var restaurants = trip.days[whichDay-1].restaurants;
-      for(i=0, len=restaurants.length; i<len; i++) {
-        if(id===restaurants[i]) {
-          curDay.restaurants = restaurants.slice(0,i).concat(restaurants.slice(i+1));
-        }
+    for(i=0, len=searchArr.length; i<len; i++) {
+      if(id===searchArr[i]) {
+        console.log(searchArr.length);
+        trip.days[whichDay-1][searchKey] = searchArr.slice(0,i).concat(searchArr.slice(i+1));
+        times_found++;
       }
     }
     populateList();
-    var twice=false;
-    if(type==="restaurant") {
-      for(i=0,len=curDay.restaurants.length; i<len; i++) {
-        if(id===curDay.restaurants[i]) {
-          twice=true;
-        }
+    if(times_found<=1) {
+      day_markers[id].setMap(null);
+      delete day_markers[id];
+    }
+  });
+
+  $("#dynamic_fill").on('click', '#removeDay', function(event) {
+    event.preventDefault();
+    var dayNum = $(this).attr("data-id") - 1;
+    for(var key in all_markers[dayNum]) {
+      if(all_markers[dayNum].hasOwnProperty(key)) {
+        all_markers[dayNum][key].setMap(null);
       }
     }
-    if(twice===false) {
-      all_markers[whichDay-1][id].setMap(null);
-      delete all_markers[whichDay-1][id];
+    all_markers.splice(dayNum, 1);
+    trip.days.splice(dayNum, 1);
+    $("#days_group:last-child").remove();
+    if(dayNum>=trip.days.length) {
+      whichDay = trip.days.length-1;
+      $("#days_group .btn-active").removeClass(".btn-active");
+      $("#days_group:last-child").addClass(".btn-active");
     }
+    populateList();
   });
 
 }); 
 
-var getProperty = function(category, id, key) {
-  if(category==="hotels") {
-    for(var i=0, len=all_hotels.length; i<len;i++) {
-      if(all_hotels[i]._id===id) {
-        return all_hotels[i][key];
+var getProperty = function(category, id, key, on_place) {
+  switch(category) {
+    case "hotels":
+      var searchArr = all_hotels;
+      break;
+    case "activities":
+      searchArr = all_activities;
+      break;
+    case "restaurants":
+      searchArr = all_restaurants;
+      break;
+  }
+  if(!on_place) {
+    for(var i=0, len=searchArr.length; i<len;i++) {
+      if(searchArr[i]._id===id) {
+        return searchArr[i][key];
+      }
+    }
+  } else {
+    console.log(searchArr, category);
+    for(var i=0, len=searchArr.length; i<len;i++) {
+      if(searchArr[i]._id===id) {
+        var coords = searchArr[i]["place"][0][key];
+        return new google.maps.LatLng(coords[0], coords[1]);
       }
     }
   }
-  else if(category==="activities") {
-    for(var i=0, len=all_activities.length; i<len;i++) {
-      if(all_activities[i]._id===id) {
-        return all_activities[i][key];
-      }
-    }
-  }
-  else if(category==="restaurants") {
-    for(var i=0, len=all_restaurants.length; i<len;i++) {
-      if(all_restaurants[i]._id===id) {
-        return all_restaurants[i][key];
-      }
-    }
-  }
+
 };
 
+//Draw day itinerary
 var populateList = function() {
+  $("#dynamic_fill").html("");
   var current_day = trip.days[whichDay-1];
-  var list = ["<h2>Plan for Day " + whichDay + "</h2><ul>"];
+  var list = ["<p><h2>Plan for Day " + whichDay + "</h2><a href='#' data-id='" + whichDay + "' id='removeDay'>Remove Day</a></p><ul>"];
   var name = getProperty("hotels", current_day.hotel, "name");
   if(current_day.hotel!==undefined) {
     list.push("<li data-id='hotel'><h3>Hotel</h3><p>" + name 
@@ -210,8 +236,9 @@ var populateList = function() {
     for(i=0,len=activities.length; i<len;i++) {
       name = getProperty("activities", activities[i], "name");
       list.push("<p>" + name + 
-        " <a href='#' data-id='" + activities[i] + "' data-type='activity' class='btn-remove'>Remove</a></p></li>");
+        " <a href='#' data-id='" + activities[i] + "' data-type='activity' class='btn-remove'>Remove</a></p>");
     }
+    list.push("</li>");
   }
   var restaurants = current_day.restaurants;
   if(restaurants.length>0) {
@@ -219,36 +246,10 @@ var populateList = function() {
     for(i=0,len=restaurants.length; i<len;i++) {
       name = getProperty("restaurants", restaurants[i], "name");
       list.push("<p>" + name + 
-        " <a href='#' data-id='" + restaurants[i] + "' data-type='restaurant' class='btn-remove'>Remove</a></p></li>");
+        " <a href='#' data-id='" + restaurants[i] + "' data-type='restaurant' class='btn-remove'>Remove</a></p>");
     }
+    list.push("</li>");
   }
   list.push("</ul>");
-  $("#dynamic_fill").html("");
   $("#dynamic_fill").append(list.join(""));
-};
-
-
-var getLocation = function(category, id) {
-  if(category==="hotels") {
-    for(var i=0, len=all_hotels.length; i<len;i++) {
-      if(all_hotels[i]._id===id) {
-        var coords = all_hotels[i]["place"][0]["location"];
-      }
-    }
-  }
-  else if(category==="activities") {
-    for(i=0, len=all_activities.length; i<len;i++) {
-      if(all_activities[i]._id===id) {
-        coords = all_activities[i]["place"][0]["location"];
-      }
-    }
-  }
-  else if(category==="restaurants") {
-    for(i=0, len=all_restaurants.length; i<len;i++) {
-      if(all_restaurants[i]._id===id) {
-        coords = all_restaurants[i]["place"][0]["location"];
-      }
-    }
-  }
-  return new google.maps.LatLng(coords[0], coords[1]);
 };
